8569ed440a77bae552afb9b9eb6ba629f35ba6ce
GhMalta <gui021096@gmail.com>
2018-11-01 13:34:10 -0300

    Comecando a implementar ProcessadorEstatistico

---
+++ src/Program/ProcessadorEstatistico/ProcessadorEstatistico.java
@@ -0,0 +1,5 @@
+package Program.ProcessadorEstatistico;
+
+public class ProcessadorEstatistico {
+
+}
---
+++ src/Tests/TestProcessadorEstatistico/TestProcessadorEstatistico.java
@@ -0,0 +1,14 @@
+package Tests.TestProcessadorEstatistico;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestProcessadorEstatistico {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
