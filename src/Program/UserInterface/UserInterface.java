3a2879014c20e5c82c4dbb7d70e9933bae6b3f73
GhMalta <gui021096@gmail.com>
2018-11-04 20:15:58 -0200

    [Implementacao+Modificacao] UserInterface:
    	Metodos modificados:
    			- public int pedeInt(String pedido)
    	Metodos implementados:
    			- private int pedeInt(Scanner input)
    			- public ArrayList<Integer> pedeArrayInt(String pedido)
    
    	Notas:
    		(1) O metodo privado foi criado para centralizar chamadas dos outros 2 metodos publicos
    		listados acima.

--- src/Program/UserInterface/UserInterface.java
+++ src/Program/UserInterface/UserInterface.java
@@ -1,5 +1,6 @@
 package Program.UserInterface;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UserInterface {
@@ -18,17 +19,28 @@ public class UserInterface {
 		
 		return entry;
 	}
+		
 	
 	public int pedeInt(String pedido) {
 		Scanner input = new Scanner(System.in);
-		String entry;
-		boolean entradaAceita;
-		int num = -1;
+		
+		int num = 0;
 		
 		System.out.println("Informe " + pedido);
+		num = this.pedeInt(input);
+		input.close();
+		return num;
+	}
+	
+	
+	private int pedeInt(Scanner input){
+		String entry;
+		boolean entradaAceita;
+		int num = 0;
+		
 		do {
+			entradaAceita = true;
 			try {
-					entradaAceita = true;
 					entry = input.nextLine();
 					num = Integer.parseInt(entry);
 				} catch (java.lang.NumberFormatException e) {
@@ -36,11 +48,27 @@ public class UserInterface {
 					System.out.println("Valor invalido. Digite novamente, por favor: ");
 				}
 		}while(!entradaAceita);
-		input.close();
+		
 		return num;
 	}
 	
-	
+	public ArrayList<Integer> pedeArrayInt(String pedido){
+		ArrayList<Integer> nums = new ArrayList<>();
+		Scanner input = new Scanner(System.in);
+		String resp="y";
+		
+		System.out.println("Informe " + pedido);
+		do {
+			nums.add(this.pedeInt(input));
+			System.out.println("[enter] Continuar \n[x] Sair");
+			resp = input.nextLine();
+			//System.out.println(resp);
+		}while(!resp.equals("x"));
+		
+		input.close();
+
+		return nums;
+	}
 	
 	
 }
