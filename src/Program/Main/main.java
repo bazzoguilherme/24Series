f50beffe75bfe5aa73e0ec11b5585366aa464519
Nicolas Duranti <nicolas.duranti@hotmail.com>
2018-11-04 20:54:49 -0200

    Merge

--- src/Program/Main/main.java
+++ src/Program/Main/main.java
@@ -1,12 +1,10 @@
 package Program.Main;
 
 import Program.Controle_Midias.Catalogo;
-<<<<<<< HEAD
 import Program.Controle_Midias.Repositorio;
 import Program.GerenciadorAcoes.GerenciadorAcoesAdm;
-=======
 import Program.UserInterface.UserInterface;
->>>>>>> f0968c05c8682393aa9191a3aadb0881384d243d
+
 
 public class main {
 	public static Catalogo catalogo = new Catalogo();
