7f86e441867e25d47899ad973c0aa2916d06f03d
Guilherme Bazzo <bazzoguilherme@gmail.com>
2018-11-12 23:46:06 -0200

    Erro Implementacao Recomendador - rever

--- src/Program/Recomendador/Recomendador.java
+++ src/Program/Recomendador/Recomendador.java
@@ -1,16 +1,68 @@
 package Program.Recomendador;
 
+import Program.Controle_Midias.Catalogo;
+import Program.Filtros.FiltroGeral;
+import Program.Filtros.FiltroRepositorio;
+import Program.Midias.Midia;
 import Program.Midias.Registro;
 import Program.ProcessadorEstatistico.ProcessadorEstatistico;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Set;
 
 
 public class Recomendador {
+    private static final Double NOTAFILTRO = 7.0;
+    private static final int QTDEFILTRORANKING = 10;
 
     public Recomendador(){
 
     }
+
+//    public ArrayList<Midia> recomendarParaUsuario(ArrayList<Registro> historico, Catalogo catalogo){
+//        ArrayList<Midia> midiasCatalogo = this.valuesHashtable(catalogo.getSeries());
+//        midiasCatalogo.addAll(this.valuesHashtable(catalogo.getFilmes()));
+//
+//        FiltroGeral filtroGeral = new FiltroGeral();
+//        ArrayList<Midia> naoAssistidas = filtroGeral.filtraPorNaoAssistidas(this.castRegToMidia(historico), midiasCatalogo);
+//
+//        FiltroRepositorio filtroRepositorio = new FiltroRepositorio();
+//        ArrayList<Midia> midiasAnalise = filtroRepositorio.filtraPorNota(NOTAFILTRO, historico);
+//        FiltroRepositorio.ordenaPorNota(this.castMidiaToReg(midiasAnalise));
+//        midiasAnalise = filtroGeral.filtraPorRanking(QTDEFILTRORANKING, midiasAnalise);
+//
+//        ArrayList<String> generosPossiveis = this.analisaGenero(this.castRegToMidia(midiasAnalise));
+//
+//
+//    }
+
+    private ArrayList<Midia> castRegToMidia(ArrayList<Registro> registros){
+        ArrayList<Midia> midias = new ArrayList<>();
+        for(Registro reg : registros){
+            midias.add((Midia) reg);
+        }
+        return midias;
+    }
+
+    private ArrayList<Registro> castMidiaToReg(ArrayList<Midia> midias){
+        ArrayList<Registro> registros = new ArrayList<>();
+        for(Midia midia : midias){
+            registros.add((Registro) midia);
+        }
+        return registros;
+    }
+
+
+    private ArrayList<Midia> valuesHashtable(Hashtable<String, Midia> midias){
+        ArrayList<Midia> midiasLista = new ArrayList<>();
+        Set<String> keyMidias = midias.keySet();
+        for (String key : keyMidias){
+            midiasLista.add(midias.get(key));
+        }
+        return midiasLista;
+    }
+
 
     public ArrayList<String> analisaGenero(ArrayList<Registro> registros){
         ArrayList<String> contadorGenero = new ArrayList<>();
